4115c9e5ca5ab4914df46071dc3f95cf54db38cc
Administrator <sanketmdesai@gmail.com>
2017-09-08 08:14:12 +0530

    Stage 2 : Introduction to variables

--- my1.py
+++ my1.py
@@ -1,3 +1,19 @@
 print("entering main")
 
+#variable creation
+i=10
+j="Helloworld"
+k=45.9
+l= 'c'
+m=True
+
+
+#printing a variable and it's type
+
+print("value of i is ",i," type of i is ",type(i))
+print("value of j is ",j," type of j is ",type(j))
+print("value of k is ",k," type of k is ",type(k))
+print("value of l is ",l," type of l is ",type(l))
+print("value of m is ",m," type of m is ",type(m))
+
 print("leaving main")
